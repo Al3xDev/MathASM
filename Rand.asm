ce580e33bc1a084c77e88bf76948bcf0c4d6a50c
Stoica Alexandru Gabriel <capitanulrex98@yahoo.com>
2016-02-10 19:05:21 +0200

    Create Rand.asm

---
+++ Rand.asm
@@ -0,0 +1,24 @@
+ global experimentalRAND
+
+section .text
+
+experimentalRAND:
+
+	mov rbx,0
+	mov rbx,rsi
+	sub rbx,rdi
+
+	cmp rbx,0
+	jne end_div0
+	mov rbx,1
+end_div0:
+
+	rdtsc
+	mov rdx,0
+
+	div rbx
+
+	mov rax,rdx
+	add rax,rdi
+
+	ret
